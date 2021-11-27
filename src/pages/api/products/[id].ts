2704de8ddc7b26eb50dd80a9df06a9798f664855
souz4s <psouza.1650@gmail.com>
2021-11-27 03:30:31 -0300

    api route to show products by id in database

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -4,13 +4,25 @@ import prisma from "../../../../lib/prisma";
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     const productId = req.query.id
 
-    if (req.method === 'DELETE') {
+    if (req.method === 'GET') {
+        handleGET(productId, res)
+    }else if (req.method === 'DELETE') {
         handleDELETE(productId, res)
     } else {
         throw new Error(
             `The HTTP ${req.method} method is not supported at this route.`
         )
     }
+}
+
+//GET /api/products/:id
+async function handleGET(productId, res) {
+    const products = await prisma.products.findUnique({
+        where: {
+            id: Number(productId)
+        }
+    })
+    res.json(products)
 }
 
 //DELETE /api/products/:id
