e4bf9f4cffcc0b67e06182651bb00982c36a6b3a
souz4s <psouza.1650@gmail.com>
2021-11-29 00:23:08 -0300

    Footer component added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Footer />
     </div>
   )
 }
